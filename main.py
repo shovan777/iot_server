0663a82024fcef595ba7055cfe69f2ec7fcc8a0c
shovan777 <raj.shrestha778@gmail.com>
2024-03-19 07:10:57 +0545

    Store sensor data in mongo collection.

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ app = FastAPI(
     title="Data collection API",
     summary="A simple application collecting data from iot devices."
 )
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://127.0.0.1:27017/fire?retryWrites=true&w=majority")
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://127.0.0.1:27017/fire"+
+                                                "?retryWrites=true&w=majority")
 db = client.fire
 data_col = db.get_collection("myCollection")
 
-print(db.list_collection_names())
+# print("*********",db.list_collection_names())
 
 # class DeviceModel(BaseModel):
 #     id: Optional[PyObjectId] = Field(alias="_id", default=None)
@@ -19,8 +20,21 @@ class DataModel(BaseModel):
     temp: int
     humidity: float
 
-@app.get("/data/{val}")
+@app.get("/data/{dev_id}")
 async def get_data(val: int):
+    """Get paginated sensor data from data collection.
+    Order the data from latest to oldest.
+
+    Parameters
+    ----------
+    val : int
+        _description_
+
+    Returns
+    -------
+    _type_
+        _description_
+    """    
     data = {"fire": val}
     print(f"******{await db.list_collection_names()}")
     return data
@@ -30,4 +44,6 @@ async def update_data(dev_id: int, data: DataModel):
     data_dict = data.model_dump()
     print(f'id: {dev_id} with data {data}')
     data_dict.update({"id": dev_id})
-    return data_dict
+    new_data = await data_col.insert_one(data_dict)
+    print("****",new_data)
+    return {"message": "success"}
