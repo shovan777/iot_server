d572ae9c59792853c268b0b1fa4ffba0033c49ce
shovan777 <raj.shrestha778@gmail.com>
2024-06-07 02:31:51 +0545

    Read dht data and send it with http request.

--- base_http/base_http.ino
+++ base_http/base_http.ino
@@ -11,8 +11,11 @@
 #include <WiFiMulti.h>
 
 #include <HTTPClient.h>
+#include "DHT.h"
 
 #define USE_SERIAL Serial
+#define DHTPIN 4
+#define DHTTYPE DHT11
 
 WiFiMulti wifiMulti;
 
@@ -62,6 +65,8 @@ void setup() {
     }
 
     wifiMulti.addAP("shovanraj153", "shrestha@1995");
+    USE_SERIAL.printf("starting dht sensor");
+    dht.begin();
 
 //    if((wifiMulti.run() == WL_CONNECTED)) {
 //      
@@ -70,7 +75,47 @@ void setup() {
 
 }
 
+float* measureDHT() {
+    delay(5000);
+    static float dataArr[2];
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    float f = dht.readTemperature(true);
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+    }
+
+    // Compute heat index in Fahrenheit (the default)
+    float hif = dht.computeHeatIndex(f, h);
+    // Compute heat index in Celsius (isFahreheit = false)
+    float hic = dht.computeHeatIndex(t, h, false);
+
+    dataArr[0] = hif;
+    dataArr[1] = hic;
+
+    Serial.print(F("Humidity: "));
+    Serial.print(h);
+    Serial.print(F("%  Temperature: "));
+    Serial.print(t);
+    Serial.print(F("°C "));
+    Serial.print(f);
+    Serial.print(F("°F  Heat index: "));
+    Serial.print(hic);
+    Serial.print(F("°C "));
+    Serial.print(hif);
+    Serial.println(F("°F"));
+    return dataArr;
+}
+
 void loop() {
+    float* dataArr = measureDHT();
     // wait for WiFi connection
     if((wifiMulti.run() == WL_CONNECTED)) {
 
@@ -86,7 +131,10 @@ void loop() {
 
         http.addHeader("Content-Type", "application/json");
         
-        String httpRequestData = "{\"temp\":\"18\", \"humidity\":\"0.5\"}";
+        // String httpRequestData = "{\"temp\":\"18\", \"humidity\":\"0.5\"}";
+        char *httpRequestData = (char*)malloc(25 * sizeof(char));
+
+        sprintf(httpRequestData, "{\"temp\":\"%f\",\"humidity\":\"%f\"}", p[0], p[1]);
         
 
         USE_SERIAL.print("[HTTP] GET...\n");
